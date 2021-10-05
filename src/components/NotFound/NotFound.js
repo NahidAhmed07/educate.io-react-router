acab54d9c47423727bea4134107f06fafb95bb9d
NahidAhmed07 <mdnahidahmed2002@gmail.com>
2021-10-05 10:34:17 +0600

    not found img added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import '../Hero/Hero.css';
 
 const NotFound = () => {
   return (
-    <div className='not-found'>
-      <div className='not-found-text'>
-        <h1>Page not Found</h1>
-      </div>
+    <div className="not-found container-fluid">
+      <Container>
+        <div >
+          <Row>
+            <Col>
+              <img
+                
+                className='img-fluid'
+                height='90vh'
+                src="https://assets.materialup.com/uploads/c13818e8-9e42-4f4d-b657-38743a81b270/preview.gif"
+                alt=""
+              />
+            </Col>
+          </Row>
+        </div>
+      </Container>
     </div>
   );
 };
